ad948c2f32d6c18f10f5461269f4ea57b64ab075
mberganz728 <124304860+mberganz728@users.noreply.github.com>
2023-03-30 14:37:31 +0200

    0

--- get_next_line.c
+++ get_next_line.c
@@ -6,39 +6,64 @@
 /*   By: mberganz <mberganz@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 10:56:14 by mberganz          #+#    #+#             */
-/*   Updated: 2023/03/29 14:02:20 by mberganz         ###   ########.fr       */
+/*   Updated: 2023/03/30 14:36:45 by mberganz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
+	char		*buffer;
+	static char	*line;
+	int			read_bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	buffer = (char *)ft_calloc((BUFFER_SIZE + 1) * (sizeof(char)));
 	if (!buffer)
 		return (0);
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	
+
 }
-/*
+
 #include <stdio.h>
 int	main()
 {
-	char *line;
-	int	fd1;
-	int	fd2;
-	int	fd3;
-	fd1 = open("tests/test.txt", O_RDONLY);
-	fd2 = open("tests/test2.txt", O_RDONLY);
-	fd3 = open("tests/test3.txt", O_RDONLY);
-	line = printf("%s\n", get_next_line(fd1));
-	line = printf("%s\n", get_next_line(fd2));
-	line = printf("%s\n", get_next_line(fd3));
+	char	*line;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+
+	fd1 = open("test.txt", O_RDONLY);
+	fd2 = open("test2.txt", O_RDONLY);
+	fd3 = open("test3.txt", O_RDONLY);
+
+	if (fd1 == -1 || fd2 == -1 || fd3 == -1)
+	{
+		printf("Error al abrir el archivo\n");
+		return (1);
+	}
+	while ((line = get_next_line(fd1)))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
 	close(fd1);
+
+	while ((line = get_next_line(fd2)))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
 	close(fd2);
+
+	while ((line = get_next_line(fd3)))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
 	close(fd3);
+
 	return (0);
-}*/
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mberganz <mberganz@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 11:01:13 by mberganz          #+#    #+#             */
-/*   Updated: 2023/03/29 13:58:30 by mberganz         ###   ########.fr       */
+/*   Updated: 2023/03/30 14:17:08 by mberganz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stddef.h>
+# include <fcntl.h>
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 1947
